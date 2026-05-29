803d12ec09a053ca0ecb31ded892aade982aa4ef
Malcolm <malhendricks@gmail.com>
2026-05-29 23:58:52 +0900

    Switched pin locations for ease in actual physical application

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include <Arduino.h>
 
-const byte GREEN_LED_PIN = 10;
-const byte YELLOW_LED_PIN = 11;
-const byte RED_LED_PIN = 12;
+const byte GREEN_LED_PIN = 2;
+const byte YELLOW_LED_PIN = 3;
+const byte RED_LED_PIN = 4;
 
-const byte GREEN_LED_PIN_SWITCH = 2;
-const byte YELLOW_LED_PIN_SWITCH = 3;
-const byte RED_LED_PIN_SWITCH = 4;
-
+const byte GREEN_LED_PIN_SWITCH = 10;
+const byte YELLOW_LED_PIN_SWITCH = 11;
+const byte RED_LED_PIN_SWITCH = 12;
 void setup() {
   pinMode(GREEN_LED_PIN, OUTPUT);
   pinMode(YELLOW_LED_PIN, OUTPUT);
